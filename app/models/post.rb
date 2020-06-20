23d46b83be58e51bc9d94d9e774fae15e58b1df0
marsha <marshaannon@gmail.com>
2020-06-20 17:58:34 -0400

    Fix validation (uniqueness)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
     #has_many :comments
 
-    validates :title, presence: true, unique: true
-    validates :content, presence: true, unique: true
+    validates :title, presence: true, uniqueness: true
+    validates :content, presence: true, uniqueness: true
     validates :author_name, presence: true
 
     scope :author, -> (author_name) { where("author_name like ?", "%#{author_name}%")}
+
+
 end
