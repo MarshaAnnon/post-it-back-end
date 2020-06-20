189339fc30a05a176c2b5a4487df4d12a0130a48
marsha <marshaannon@gmail.com>
2020-06-20 18:52:29 -0400

    Stub out actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,50 +1,41 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :update, :destroy]
+  before_action :set_post, only: [:show, :update]
 
-  # GET /posts
   def index
-    @posts = Post.all
+    posts = Post.all
 
-    render json: @posts
+    render json: posts
   end
 
-  # GET /posts/1
   def show
-    render json: @post
+    render json: post
   end
 
-  # POST /posts
   def create
-    @post = Post.new(post_params)
+    post = Post.new(post_params)
 
-    if @post.save
-      render json: @post, status: :created, location: @post
+    if post.save
+      render json: post#, status: :created, location: post
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: post.errors#, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /posts/1
   def update
-    if @post.update(post_params)
-      render json: @post
+    if post.update(post_params)
+      render json: post
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: post.errors#, status: :unprocessable_entity
     end
   end
 
-  # DELETE /posts/1
-  def destroy
-    @post.destroy
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_post
-      @post = Post.find(params[:id])
+      post = Post.find_by_id(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def post_params
       params.require(:post).permit(:title, :content, :author_name, :likes)
     end
