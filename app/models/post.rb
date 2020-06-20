6641f49eab0213ff606faa3bce5b1d08562f4ecf
marsha <marshaannon@gmail.com>
2020-06-20 17:46:46 -0400

    Add scope method for author

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
     validates :title, presence: true, unique: true
     validates :content, presence: true, unique: true
     validates :author_name, presence: true
+
+    scope :author, -> (author_name) { where("author_name like ?", "%#{author_name}%")}
 end
