bb760cf4aac0a9ca3f2f89c2ccc8db22a382d7cc
marsha <marshaannon@gmail.com>
2020-06-20 18:15:40 -0400

    Add default scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,6 @@ class Post < ApplicationRecord
     validates :content, presence: true, uniqueness: true
     validates :author_name, presence: true
 
+    default_scope { order(created_at: :desc) }
     scope :author, -> (author_name) { where("author_name like ?", "%#{author_name}%")}
-
-
 end
