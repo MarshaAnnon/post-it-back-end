3e8ae8debf90bdca7a942a6f0fcb19e423923cb1
marsha <marshaannon@gmail.com>
2020-06-21 18:02:59 -0400

    Stub out controller with CommentSerializer and options

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,51 +1,44 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :update, :destroy]
 
-  # GET /comments
+  #before_action :set_comment, only: [:show, :update, :destroy]
+
   def index
-    @comments = Comment.all
-
-    render json: @comments
+    comments = Comment.all
+    options = {
+      include: [:post]
+    }
+    render json: CommentSerializer.new(comments, options)
   end
 
-  # GET /comments/1
   def show
-    render json: @comment
+    comment = Comment.find_by_id(params[:id])
+    options = {
+      include: [:post]
+    }
+    render json: CommentSerializer.new(comment, options)  
   end
 
-  # POST /comments
   def create
-    @comment = Comment.new(comment_params)
+    comment = Comment.new(comment_params)
 
-    if @comment.save
-      render json: @comment, status: :created, location: @comment
+    if comment.save
+      options = {
+        include: [:post]
+      }
+      render json: CommentSerializer.new(comment, options)#, status: :created, location: comment
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: comment.errors#, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /comments/1
-  def update
-    if @comment.update(comment_params)
-      render json: @comment
-    else
-      render json: @comment.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /comments/1
-  def destroy
-    @comment.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def comment_params
-      params.require(:comment).permit(:content, :name, :post_id)
-    end
+  #def set_comment
+  #  comment = Comment.find_by_id(params[:id])
+  #end
+
+  def comment_params
+    params.require(:comment).permit(:content, :name, :post_id)
+  end
+
 end
