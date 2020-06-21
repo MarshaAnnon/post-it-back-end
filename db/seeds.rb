85a3d666bdc5d6077211ae83fa496f20110958fb
marsha <marshaannon@gmail.com>
2020-06-21 17:30:26 -0400

    Added seed data for comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,12 @@ Post.create(
     author_name: Faker::Internet.username,
     likes: rand(20)
 )
+
+Comment.create(
+    content: Faker::Lorem.sentence(word_count: 150),
+    name: Faker::Name.first_name,
+    post: Post.all.sample
+
+)
+
 end
