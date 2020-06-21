3aa4a9c309a6839b76c48e0d89505b5229abf4d5
marsha <marshaannon@gmail.com>
2020-06-21 17:48:08 -0400

    Added post_params to Post.new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new
+    post = Post.new(post_params)
     if post.save
       #options = {
         #include: [:comment]
