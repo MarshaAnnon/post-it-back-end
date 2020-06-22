99687ec438cc155401c67461228d23808b65f1c7
marsha <marshaannon@gmail.com>
2020-06-22 13:09:41 -0400

    Customize comment routes for show and create only

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   resources :comments, only: [:show, :create]
   resources :posts, only: [:index, :show, :create, :update]
 
-  #resources :posts, only: [:show] do
-  #  resources :comments, only: [:index, :create]
-  #end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
