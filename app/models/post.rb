bd093a8ed9019d29c5a19d8acf03bb2d9c875eb7
marsha <marshaannon@gmail.com>
2020-06-22 11:25:48 -0400

    added accepts_nested_attributes_for :comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
     validates :content, presence: true, uniqueness: true
     validates :author_name, presence: true
 
+    accepts_nested_attributes_for :comments, allow_destroy: true
+
     default_scope { order(created_at: :desc) }
     scope :author, -> (author_name) { where("author_name like ?", "%#{author_name}%")}
 end
