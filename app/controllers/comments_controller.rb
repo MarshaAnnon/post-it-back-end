b9014e3dacb901754b7a37a21642c25a9426493d
marsha <marshaannon@gmail.com>
2020-06-23 15:29:19 -0400

    Changed local variables to instance variables, took out before action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,26 @@
 class CommentsController < ApplicationController
 
-  #before_action :set_comment, only: [:show, :update, :destroy]
+  
 
   def show
-    comment = Comment.find_by_id(params[:id])
+    @comment = Comment.find_by_id(params[:id])
     options = {
       include: [:post]
     }
-    render json: comment.as_json(include: {post: {only: [:id, :title, :content, :author_name, :likes, :created_at]}})  
+    render json: @comment.as_json(include: {post: {only: [:id, :title, :content, :author_name, :likes, :created_at]}})  
   end
   
   def create
-    comment = Comment.new(comment_params)
+    @comment = Comment.new(comment_params)
 
-    if comment.save
-      render json: comment.as_json(include: {post: {only: [:id, :title, :content, :author_name, :likes, :created_at]}})
+    if @comment.save
+      render json: @comment.as_json(include: {post: {only: [:id, :title, :content, :author_name, :likes, :created_at]}})
     else
-      render json: comment.errors#, status: :unprocessable_entity
+      render json: @comment.errors#, status: :unprocessable_entity
     end
   end
 
   private
-
-  #def set_comment
-  #  comment = Comment.find_by_id(params[:id])
-  #end
 
   def comment_params
     params.require(:comment).permit(:id, :content, :name, :post_id)
