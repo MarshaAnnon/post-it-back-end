3174ed3ea81913f4f55427c1088d15287cdf6e7c
marsha <marshaannon@gmail.com>
2020-06-23 15:27:14 -0400

    Changed local variables to instance variable, uncommented before action and set_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,40 +1,41 @@
 class PostsController < ApplicationController
-  #before_action :set_post, only: [:show, :update]
+  
+  before_action :set_post, only: [:show, :update]
   
   def index
-    posts = Post.all
-    render json: posts.as_json(include: {comments: {only: [:id, :content, :name, :created_at]}})
+    @posts = Post.all
+    render json: @posts.as_json(include: {comments: {only: [:id, :content, :name, :created_at]}})
   end
   
   def show
-    post = Post.find_by_id(params[:id])
-    render json: post.as_json(include: {comments: {only: [:id, :content, :name, :created_at]}})
+    @post = Post.find_by_id(params[:id])
+    render json: @post.as_json(include: {comments: {only: [:id, :content, :name, :created_at]}})
   end
 
   def create
-    post = Post.new(post_params)
-    if post.save
-      render json: post.as_json(include: {comments: {only: [:id, :content, :name, :created_at]}})
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post.as_json(include: {comments: {only: [:id, :content, :name, :created_at]}})
         #, status: :created, location: post
     else
-      render json: post.errors#, status: :unprocessable_entity
+      render json: @post.errors#, status: :unprocessable_entity
     end
   end  
 
   def update
-    if post.update(post_params)
-      render json: post.as_json(include: {comments: {only: [:id, :content, :name, :created_at]}})
+    if @post.update(post_params)
+      render json: @post.as_json(include: {comments: {only: [:id, :content, :name, :created_at]}})
         #, status: :created, location: post
     else
-      render json: post.errors#, status: :unprocessable_entity
+      render json: @post.errors#, status: :unprocessable_entity
     end
   end
 
   private
     
-  #def set_post
-    #post = Post.find_by_id(params[:id])
-  #end
+  def set_post
+    @post = Post.find_by_id(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(
