725d00d9a92f6f3a9af1470da73c56c955abe36c
marsha <marshaannon@gmail.com>
2020-06-27 18:40:11 -0400

    removed attr_writer and reader for strfttime, couldn't get to work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ApplicationRecord
+    
     has_many :comments, dependent: :delete_all
 
     validates :title, presence: true, uniqueness: true
     validates :content, presence: true, uniqueness: true
-    validates :author_name, presence: true
 
     accepts_nested_attributes_for :comments, allow_destroy: true
 
     default_scope { order(created_at: :desc) }
-    scope :author, -> (author_name) { where("author_name like ?", "%#{author_name}%")}
+    
 end
