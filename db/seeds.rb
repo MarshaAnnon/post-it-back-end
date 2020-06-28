c7d9a448909c8914f8de2249d341d24cdde3f3b7
marsha <marshaannon@gmail.com>
2020-06-28 18:57:03 -0400

    changed seed data to look better in the browser

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 10.times do 
-Post.create(
-    title: Faker::Quotes::Shakespeare.hamlet_quote,
-    content: Faker::Lorem.sentence(word_count: 250),
-    author_name: Faker::Internet.username,
-    likes: rand(20)
-)
-
-Comment.create(
-    content: Faker::Lorem.sentence(word_count: 150),
-    name: Faker::Name.first_name,
-    post: Post.all.sample
-
-)
-
+    Post.create(
+        title: Faker::Quote.robin,
+        content: Faker::Quotes::Shakespeare.hamlet_quote,
+        author_name: Faker::Internet.username,
+        likes: rand(20)
+    )
+    
+    Comment.create(
+        content: Faker::Quote.matz,
+        name: Faker::Name.first_name,
+        post: Post.all.sample
+    
+    )
 end
+   
