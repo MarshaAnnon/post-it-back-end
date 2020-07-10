7eb27d460ab86f401f5d975c9bad79ad33c243e7
marsha <marshaannon@gmail.com>
2020-07-10 16:13:57 -0400

    Fixed spacing

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
 
     validates :title, presence: true, uniqueness: true
     validates :content, presence: true, uniqueness: true
-    validates :name, presence: true
+    validates :author_name, presence: true
 
     accepts_nested_attributes_for :comments
 
