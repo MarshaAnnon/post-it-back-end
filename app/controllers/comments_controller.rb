e20879e455d3002cb1c01c8714141d3dcff69c7b
marsha <marshaannon@gmail.com>
2020-08-29 23:09:49 -0400

    fix indentation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,26 @@
 class CommentsController < ApplicationController
 
-    def show
-        @comment = Comment.find_by_id(params[:id])
-        options = {
-          include: [:post]
-        }
-        render json: @comment.as_json(include: {post: {only: [:id, :title, :content, :author_name, :likes, :created_at]}})  
-      end
+  def show
+    @comment = Comment.find_by_id(params[:id])
+    options = {
+      include: [:post]
+    }
+    render json: @comment.as_json(include: {post: {only: [:id, :title, :content:author_name, :likes, :created_at]}})  
+  end
       
-      def create
-        @comment = Comment.new(comment_params)
-        if @comment.save
-          render json: @comment.as_json(include: {post: {only: [:id, :title, :content, :author_name, :likes, :created_at]}})
-        else
-          render json: @comment.errors, status: :unprocessable_entity
-        end
-      end
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: @comment.as_json(include: {post: {only: [:id, :title, :content:author_name, :likes, :created_at]}})
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
     
-      private
+  private
     
-      def comment_params
-        params.require(:comment).permit(:id, :content, :name, :post_id)
-      end
+  def comment_params
+    params.require(:comment).permit(:id, :content, :name, :post_id)
+  end
       
 end
